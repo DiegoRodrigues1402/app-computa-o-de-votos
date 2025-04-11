22e824e5ca3f9832d438679210d837c36328b63d
Diego Rodrigues <diego.rodrigues1402@gmail.com>
2025-04-11 10:26:33 -0300

    novo repositorio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/app-computa-o-de-votos"
+  base: "/app-computacao-de-votos"
 })
